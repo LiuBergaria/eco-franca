b767048b78beaf4fafdbaded05a4f68637b4632a
Helil Barbosa Bergária <liubergaria@hotmail.com>
2021-07-15 22:05:08 -0300

    Added PressableBlock component

---
+++ src/components/PressableBlock/index.tsx
@@ -0,0 +1,58 @@
+import React, { useMemo } from 'react';
+import { TouchableOpacityProps } from 'react-native';
+
+import Colors from 'src/styles/Colors';
+
+import { Container, Title, Icon, FloatElementContainer } from './styles';
+
+interface IProps extends TouchableOpacityProps {
+  title: string;
+  icon: string;
+  lightStyle: boolean;
+  isRow?: boolean;
+  floatElement?: JSX.Element;
+}
+
+const PressableBlock = ({
+  title,
+  icon,
+  lightStyle,
+  isRow = true,
+  floatElement,
+  ...props
+}: IProps): JSX.Element => {
+  const pickedColors = useMemo(
+    () =>
+      lightStyle
+        ? {
+            backgroundColor: Colors.lightTeal,
+            iconColor: Colors.teal,
+            textColor: Colors.black,
+          }
+        : {
+            backgroundColor: Colors.teal,
+            iconColor: Colors.lightTeal,
+            textColor: Colors.white,
+          },
+    [lightStyle],
+  );
+
+  return (
+    <Container
+      activeOpacity={0.8}
+      backgroundColor={pickedColors.backgroundColor}
+      isRow={isRow}
+      {...props}
+    >
+      {floatElement && (
+        <FloatElementContainer>{floatElement}</FloatElementContainer>
+      )}
+      <Title color={pickedColors.textColor} isRow={isRow}>
+        {title}
+      </Title>
+      <Icon color={pickedColors.iconColor} size={40} name={icon} />
+    </Container>
+  );
+};
+
+export default PressableBlock;
---
+++ src/components/PressableBlock/styles.ts
@@ -0,0 +1,47 @@
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+import styled, { css } from 'styled-components/native';
+
+import Fonts from 'src/styles/Fonts';
+
+interface IContainer {
+  backgroundColor: string;
+  isRow: boolean;
+}
+
+interface ITitle {
+  color: string;
+  isRow: boolean;
+}
+
+export const Container = styled.TouchableOpacity<IContainer>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+
+  flex-grow: 1;
+  height: 140px;
+  padding: 24px;
+  border-radius: 4px;
+
+  flex-direction: ${({ isRow }) => (isRow ? 'row' : 'column-reverse')};
+  justify-content: ${({ isRow }) => (isRow ? 'space-between' : 'center')};
+  align-items: center;
+`;
+
+export const Title = styled.Text<ITitle>`
+  color: ${({ color }) => color};
+  font-size: ${({ isRow }) => (isRow ? '20px' : '14px')};
+  font-family: ${Fonts.Inter.Medium};
+
+  ${({ isRow }) =>
+    !isRow &&
+    css`
+      margin-top: 8px;
+    `}
+`;
+
+export const Icon = styled(FontAwesome5Icon)``;
+
+export const FloatElementContainer = styled.View`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+`;
