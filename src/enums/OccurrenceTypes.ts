d7071aaed332fb6c8a47b76e2ba42069fe41460c
Helil Barbosa Bergária <liubergaria@hotmail.com>
2021-07-17 15:41:36 -0300

    feat: Created occurrence status and types enums and utils to extract informations

---
+++ src/enums/OccurrenceTypes.ts
@@ -0,0 +1,10 @@
+enum OccurrenceTypes {
+  DescarteIrregularDeResiduos,
+  Desmatamento,
+  LoteamentoIrregular,
+  UsoIndevidoDeAreaPublica,
+  MausTratosContraAnimais,
+  AbandonoDeAnimais,
+}
+
+export default OccurrenceTypes;
