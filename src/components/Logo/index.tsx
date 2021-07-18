45295934ec5b39acf354e91b38217bb631d09704
Helil Barbosa Bergária <liubergaria@hotmail.com>
2021-07-18 02:47:33 -0300

    fix: Removed mocked toggle theme logo button

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageProps, TouchableOpacity } from 'react-native';
+import { ImageProps } from 'react-native';
 
 import LogoImage from 'src/assets/logo-black.png';
 import DarkLogoImage from 'src/assets/logo-white.png';
@@ -10,15 +10,11 @@ import { Image } from './styles';
 type IProps = Omit<ImageProps, 'source'>;
 
 const Logo = (props: IProps): JSX.Element => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   const source = theme === 'light' ? LogoImage : DarkLogoImage;
 
-  return (
-    <TouchableOpacity onPress={toggleTheme}>
-      <Image source={source} {...props} />
-    </TouchableOpacity>
-  );
+  return <Image source={source} {...props} />;
 };
 
 export default Logo;
