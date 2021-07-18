9aaea05a30eb084ee59d82b1fe001e5c9b78b7f3
Helil Barbosa Bergária <liubergaria@hotmail.com>
2021-07-18 15:17:18 -0300

    feat: Structured auth context

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,19 +1,110 @@
-import React, { createContext, useContext } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+  useMemo,
+} from 'react';
 
-interface IAuthContextData {}
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const AuthContext = createContext<IAuthContextData>({});
+import api from '../services/api';
+
+interface IAuthState {
+  token: string;
+  user: {
+    first_name: number;
+    last_name: string;
+  };
+}
+
+interface IAuthContextData {
+  user: {
+    first_name: number;
+    last_name: string;
+  };
+  signIn(informations: IAuthState): Promise<void>;
+  signOut(): Promise<void>;
+  isAuthenticated: boolean;
+  isLoaded: boolean;
+}
+
+export const AuthContext = createContext<IAuthContextData>(
+  {} as IAuthContextData,
+);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [data, setData] = useState<IAuthState>(() => ({} as IAuthState));
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadStorageData(): Promise<void> {
+      const [[, token], [, user]] = await AsyncStorage.multiGet([
+        '@EcoFranca:token',
+        '@EcoFranca:user',
+      ]);
+
+      if (token && user) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
+        setData({ token, user: JSON.parse(user) });
+      }
+
+      setIsLoaded(true);
+    }
+
+    loadStorageData();
+  }, []);
+
+  const signIn = useCallback(async ({ token, user }: IAuthState) => {
+    await AsyncStorage.multiSet([
+      ['@EcoFranca:token', token],
+      ['@EcoFranca:user', JSON.stringify(user)],
+    ]);
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@EcoFranca:token', '@EcoFranca:user']);
+
+    delete api.defaults.headers.Authorization;
+    setData({} as IAuthState);
+  }, []);
+
+  useEffect(() => {
+    const id = api.interceptors.response.use((value) => {
+      if (value.status === 401) {
+        signOut();
+      }
+      return value;
+    });
+
+    return () => {
+      api.interceptors.response.eject(id);
+    };
+  }, [data, signOut]);
+
+  const isAuthenticated = useMemo(() => !!data.token, [data.token]);
+
+  return (
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, isAuthenticated, isLoaded }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export const useAuth = (): IAuthContextData => {
+export function useAuth(): IAuthContextData {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth must be used within a AuthProvider');
+    throw new Error('useAuth must be used within an AuthProvider');
   }
 
   return context;
-};
+}
