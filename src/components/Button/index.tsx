7d2d05280c443d7670b9fd4d7d2f8e5bc90244bf
Helil Barbosa Bergária <liubergaria@hotmail.com>
2021-07-18 15:16:50 -0300

    feat: Added button loading feature

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,12 +4,13 @@ import { TextProps, TouchableOpacityProps } from 'react-native';
 import { useTheme } from 'src/contexts/theme';
 import Colors from 'src/styles/Colors';
 
-import { Container, Title } from './styles';
+import { Container, Loader, Title } from './styles';
 
 interface IProps extends TouchableOpacityProps {
   title: string;
   colorStyle?: 'teal' | 'lightTeal' | 'red' | 'transparent';
   textStyles?: TextProps;
+  isLoading?: boolean;
 }
 
 const textColorMap = {
@@ -26,6 +27,7 @@ const Button = ({
   title,
   colorStyle = 'teal',
   textStyles,
+  isLoading,
   ...props
 }: IProps): JSX.Element => {
   const { theme } = useTheme();
@@ -37,10 +39,17 @@ const Button = ({
       : textColorMap[colorStyle];
 
   return (
-    <Container activeOpacity={0.75} {...props} color={color}>
+    <Container
+      activeOpacity={0.75}
+      {...props}
+      color={color}
+      disabled={isLoading}
+    >
       <Title style={textStyles} textColor={textColor}>
         {title}
       </Title>
+
+      {isLoading && <Loader />}
     </Container>
   );
 };
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,9 @@
+import { ActivityIndicator } from 'react-native';
+
+import { transparentize } from 'polished';
 import styled from 'styled-components/native';
 
+import Colors from 'src/styles/Colors';
 import Fonts from 'src/styles/Fonts';
 
 interface IContainerProps {
@@ -11,12 +15,15 @@ interface ITitleProps {
 }
 
 export const Container = styled.TouchableOpacity<IContainerProps>`
+  justify-content: center;
+
   width: 100%;
 
   padding: 12px;
   margin-bottom: 16px;
 
-  background-color: ${({ color }) => color};
+  background-color: ${({ disabled, color }) =>
+    disabled ? transparentize(0.2, color) : color};
 
   border-radius: 4px;
 `;
@@ -26,4 +33,12 @@ export const Title = styled.Text<ITitleProps>`
   text-align: center;
   font-size: 14px;
   font-family: ${Fonts.Inter.SemiBold};
+`;
+
+export const Loader = styled(ActivityIndicator).attrs({
+  color: Colors.white,
+  size: 'small',
+})`
+  position: absolute;
+  right: 16px;
 `;
