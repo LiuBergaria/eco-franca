681bde148ddbe10e2f5378a7b59fffc6ca7d4eca
Helil Barbosa Bergária <liubergaria@hotmail.com>
2021-09-02 21:27:33 -0300

    feat(api): Added delay in dev and baseURL based on platform

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,26 @@
+import { Platform } from 'react-native';
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL:
+    Platform.OS === 'android'
+      ? 'http://192.168.0.93:3000'
+      : 'http://localhost:3000',
   validateStatus: () => true,
   timeout: 30000,
 });
+
+if (__DEV__) {
+  function sleep(ms = 2000): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  api.interceptors.response.use(async (response) => {
+    await sleep();
+
+    return response;
+  });
+}
 
 export default api;
